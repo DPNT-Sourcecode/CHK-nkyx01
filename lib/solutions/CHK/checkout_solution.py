2a626d7a20c6a28607463e8e3222215d7737818e
sbx_user1051 <sbx_user1051@169.254.35.209>
2024-06-02 17:00:04 +0000

    Sun Jun 02 17:00:04 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -47,62 +47,80 @@ PRICE_TABLE = {
 }
 
 
-# prepare basket and return None if invalid sku in the skus
-def get_basket(skus) -> Dict[str, int]:
-    basket =  {}
-    for item in skus:
-        # Validate item in the basket if it is a valid item or not?!
-        if item not in PRICE_TABLE:
+class SpecialOffer:
+    def __init__(self, special_offers: Dict[str, Dict], offer_levels: List[str]) -> None:
+        self._special_offers = special_offers
+        self._offer_levels = offer_levels
+
+    # return all offers for the input sku,
+    # None if no offer found
+    def get_offer(self, level: str, sku: str) -> List[Dict]:
+        if level not in self._special_offers:
             return None
+        return self._special_offers[level].get(sku)
+
+    @property
+    def offer_levels(self):
+        return self._offer_levels
+
+class Basket:
+    def __init__(self, skus: str, price_table: Dict[str, int], spcial_offers: SpecialOffer) -> None:
+        self._skus = skus
+        self._price_table = price_table
+        self._spcial_offers = spcial_offers
+
+    # get basket and return None if invalid sku in the skus
+    def get_basket(self) -> Dict[str, int]:
+        basket =  {}
+        for item in self._skus:
+            # Validate item in the basket if it is a valid item or not?!
+            if item not in PRICE_TABLE:
+                return None
+            if item in basket:
+                basket[item] += 1
+            else:
+                basket[item] = 1
+        return basket
+
+    def apply_offer_to_basket(self, sku: str, offer: Dict, basket: Dict[str, int]):
+        total = 0
+        n = basket[sku] // offer[NO_OF_ITEMS]
+        if n > 0:
+            basket[sku] -= n * offer[NO_OF_ITEMS]
+            total += n * offer[PRICE]
+            if FREE in offer:
+                free_sku = offer[FREE]
+                if basket.get(free_sku, 0) > 0:
+                    basket[free_sku] -= n
+        return total
+    
+    def checkout(self):
+        total = 0
+
+        basket = self.get_basket()
+        if basket is None:
+            return -1
         
-        if item in basket:
-            basket[item] += 1
-        else:
-            basket[item] = 1
-    return basket
+        for level in self._spcial_offers.offer_levels:
+            for sku in basket:
+                offers = self._spcial_offers.get_offer(level, sku)
+                if offers is not None:
+                    for offer in offers:
+                        total += self.apply_offer_to_basket(sku, offer, basket)
 
+        for sku in basket:
+            if basket[sku] > 0:
+                total += basket[sku] * self._price_table[sku]
 
-# return all offers for the input sku,
-# None if no offer found
-def get_offer(level: str, sku: str) -> List[Dict]:
-    if level not in SPECIAL_OFFERS:
-        return None
-    return SPECIAL_OFFERS[level].get(sku)
+        return total
 
-
-def apply_offer_to_basket(sku: str, offer: Dict, basket: Dict[str, int]):
-    total = 0
-    n = basket[sku] // offer[NO_OF_ITEMS]
-    if n > 0:
-        basket[sku] -= n * offer[NO_OF_ITEMS]
-        total += n * offer[PRICE]
-        if FREE in offer:
-            free_sku = offer[FREE]
-            if basket.get(free_sku, 0) > 0:
-                basket[free_sku] -= n
-    return total
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    total = 0
-
-    basket = get_basket(skus)
-    if basket is None:
-        return -1
-    
-    for level in SPECIAL_OFFER_LEVELS:
-        for sku in basket:
-            offers = get_offer(level, sku)
-            if offers is not None:
-                for offer in offers:
-                    total += apply_offer_to_basket(sku, offer, basket)
-
-    for sku in basket:
-        if basket[sku] > 0:
-            total += basket[sku] * PRICE_TABLE[sku]
-
-    return total
+    spcial_offers = SpecialOffer(SPECIAL_OFFERS, SPECIAL_OFFER_LEVELS)
+    basket = Basket(skus, PRICE_TABLE, spcial_offers)
+    return basket.checkout()
 
 
 class CheckoutTestCase(unittest.TestCase):
