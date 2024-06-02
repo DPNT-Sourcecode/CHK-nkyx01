2d49694920093fed09f5094dd4b3636af5e869a0
sbx_user1051 <sbx_user1051@169.254.35.209>
2024-06-02 15:48:20 +0000

    Sun Jun 02 15:48:20 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,57 @@
+import unittest
 
+NO_OF_ITEMS = 'numberOfItems'
+PRICE = 'price'
+SPECIAL_OFFERS = {
+    'A': {
+        NO_OF_ITEMS: 3,
+        PRICE: 130,
+    },
+    'B': {
+        NO_OF_ITEMS: 2,
+        PRICE: 45,
+    }
+}
+
+PRICE_TABLE = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    total = 0
+    basket =  {}
+    for x in skus:
+        if x not in PRICE_TABLE:
+            return -1
+        
+        if x in basket:
+            basket[x] += 1
+        else:
+            basket[x] = 1
+    
+    for x in basket:
+        if x in SPECIAL_OFFERS:
+            special_offer_no = SPECIAL_OFFERS[x][NO_OF_ITEMS]
+            n = basket[x] // special_offer_no
+            if n > 0:
+                basket[x] -= n * special_offer_no
+                total += SPECIAL_OFFERS[x][PRICE]
+
+        if basket[x] > 0:
+            total += basket[x] * PRICE_TABLE[x]
+
+    return total
+print(__name__)
+if __name__=='__main__':
+    unittest.main()
+
+class CheckoutTestCase(unittest.TestCase):
+    def test_check_invalid(self):
+        res = checkout('ABHC')
+        self.assertEqual(res, -1)
+
