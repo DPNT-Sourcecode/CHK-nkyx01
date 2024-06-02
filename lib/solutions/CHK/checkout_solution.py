709fd8b9bf507c4d97f836680177438a3036364c
sbx_user1051 <sbx_user1051@169.254.35.209>
2024-06-02 16:27:04 +0000

    Sun Jun 02 16:27:04 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,14 +2,37 @@ import unittest
 
 NO_OF_ITEMS = 'numberOfItems'
 PRICE = 'price'
+FREE =  'free'
+
+LEVEL_1 = 'level 1'
+LEVEL_2 = 'level 2'
+
+SPECIAL_OFFER_LEVELS = [LEVEL_1, LEVEL_2]
+
+# Special offers table and offers are proitrised
 SPECIAL_OFFERS = {
-    'A': {
-        NO_OF_ITEMS: 3,
-        PRICE: 130,
+    LEVEL_1: {
+        'E': [{
+            NO_OF_ITEMS: 2,
+            PRICE: 80,
+            FREE: 'B',
+        }],
     },
-    'B': {
-        NO_OF_ITEMS: 2,
-        PRICE: 45,
+    LEVEL_2: {
+        'A': [
+            {
+                NO_OF_ITEMS: 5,
+                PRICE: 200,
+            },
+            {
+                NO_OF_ITEMS: 3,
+                PRICE: 130,
+            },
+        ],
+        'B': [{
+            NO_OF_ITEMS: 2,
+            PRICE: 45,
+        }],
     }
 }
 
@@ -20,29 +43,58 @@ PRICE_TABLE = {
     'D': 15
 }
 
+
+# prepare basket and return None if invalid sku in the skus
+def get_basket(skus):
+    basket =  {}
+    for item in skus:
+        # Validate item in the basket if it is a valid item or not?!
+        if item not in PRICE_TABLE:
+            return None
+        
+        if item in basket:
+            basket[item] += 1
+        else:
+            basket[item] = 1
+    return basket
+
+
+# return all offers for the input sku,
+# None if no offer found
+def get_offer(level, sku):
+    if level not in SPECIAL_OFFERS:
+        return None
+    return SPECIAL_OFFERS[level].get(sku)
+
+
+def apply_offer_to_basket(sku, offer, basket):
+    total = 0
+    n = basket[sku] // offer[NO_OF_ITEMS]
+    if n > 0:
+        basket[sku] -= n * offer[NO_OF_ITEMS]
+        total += n * SPECIAL_OFFERS[sku][PRICE]
+        if FREE in offer:
+            basket[offer[FREE]] -= 1
+    return total
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     total = 0
-    basket =  {}
-    for x in skus:
-        if x not in PRICE_TABLE:
-            return -1
-        
-        if x in basket:
-            basket[x] += 1
-        else:
-            basket[x] = 1
-    print(basket)
-    for x in basket:
-        if x in SPECIAL_OFFERS:
-            special_offer_no = SPECIAL_OFFERS[x][NO_OF_ITEMS]
-            n = basket[x] // special_offer_no
-            if n > 0:
-                basket[x] -= n * special_offer_no
-                total += n * SPECIAL_OFFERS[x][PRICE]
 
-        if basket[x] > 0:
+    basket = get_basket(skus)
+    if basket is None:
+        return -1
+    
+    for level in SPECIAL_OFFER_LEVELS:
+        for sku in basket:
+            offers = get_offer(level, sku)
+            if offers is not None:
+                for offer in offers:
+                    total += apply_offer_to_basket(sku, offer, basket)
+
+    for sku in basket:
+        if basket[sku] > 0:
             total += basket[x] * PRICE_TABLE[x]
 
     return total
